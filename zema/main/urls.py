5eb168d77c6c0c8e7e8e9daadcad95f3b57de62e
mcdos63 <klvgk@mail.ru>
2025-06-07 14:40:50 +0400

    n1

--- zema/main/urls.py
+++ zema/main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('test', views.test),
-    path('data', views.data),
+    path('', views.index, name='home'),
+    path('test', views.test, name='test'),
+    path('data', views.data, name='data'),
+    path('article', views.article, name='article'),
 ]
--- zema/main/views.py
+++ zema/main/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from datetime import datetime
-# Получить текущее время
-current_time = datetime.now().time()
-
 # Create your views here.
 def index(request):
     # return render(request, 'main/index.html')
-    return HttpResponse("<h1>Hello, ZёMa</h1>")
+    return render(request, 'main/index.html')
 
 def test(request):
-    return HttpResponse("<h2>TEST, ZёMa</h2>")
+    return render(request, 'main/test.html')
 
 def data(request):
-    return HttpResponse(f"<h2>DATA, ZёMa. Time: {current_time}</h2>")
+    return render(request, 'main/data.html')
+
+def article(request):
+    return render(request, 'main/article.html')
